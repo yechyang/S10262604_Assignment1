39016241e2b4db1baff02beac4af62e9550fbdf0
kohye <kohyechyang11@gmail.com>
2023-12-01 00:27:45 +0800

    Popup in Contact

---
+++ contact.js
@@ -0,0 +1,10 @@
+function showPopup() {
+    var modal = document.getElementById('myModal');
+    modal.style.display = 'block';
+}
+
+// Function to close the popup
+function closePopup() {
+    var modal = document.getElementById('myModal');
+    modal.style.display = 'none';
+}
